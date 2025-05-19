11a1e003728cd29c4b0cbaa67b6db87a9554774f
pekochan069 <pekochan069@gmail.com>
2025-05-19 23:59:51 +0900

    Initial commit

--- src/Kestrel.jl
+++ src/Kestrel.jl
@@ -1,5 +1,182 @@
+"""
+https://github.com/codereport/blackbird
+"""
 module Kestrel
 
-# Write your package code here.
+"""
+I - Identity
+"""
+function i(x)
+    x
+end
+
+"""
+K - Kestrel
+"""
+function k(x, y)
+    x
+end
+
+"""
+S - Starling
+"""
+function s(f, g)
+    (x) -> f(x, g(x))
+end
+
+"""
+B - Bluebird
+"""
+function b(f, g)
+    (x) -> f(g(x))
+end
+
+"""
+BB - Bluebird^2
+"""
+function bb(f, g, h)
+    (x) -> f(g(h(x)))
+end
+
+"""
+BBB - Bluebird^3
+"""
+function bbb(f, g, h, i)
+    (x) -> f(g(h(i(x))))
+end
+
+"""
+B1 - Blackbird
+"""
+function b1(f, g)
+    (x, y) -> f(g(x, y))
+end
+
+"""
+C - Cardinal
+"""
+function c(f)
+    (x, y) -> f(y, x)
+end
+
+"""
+D2 - Dovekie
+"""
+function d2(f, g, h)
+    (x, y) -> g(f(x), h(y))
+end
+
+"""
+KI - Kite
+"""
+function ki(x, y)
+    y
+end
+
+"""
+Phi - Phoenix
+"""
+function phi(f, g, h)
+    (x) -> g(f(x), h(x))
+end
+
+"""
+Phi1 - Pheasant
+"""
+function phi1(f, g, h)
+    (x, y) -> g(f(x, y), h(x, y))
+end
+
+"""
+Psi - Psi Bird
+"""
+function psi(f, g)
+    (x, y) -> f(g(x), g(y))
+end
+
+"""
+W - Warbler
+"""
+function w(f)
+    (x) -> f(x, x)
+end
+
+export i, k, s, b, bb, bbb, b1, c, d2, ki, phi, phi1, psi, w
+
+module Curried
+
+"""
+I - Identity
+"""
+i = x -> x
+
+"""
+K - Kestrel
+"""
+k = x -> y -> x
+
+"""
+S - Starling
+"""
+s = f -> g -> x -> f(x)(g(x))
+
+"""
+B - Bluebird
+"""
+b = f -> g -> x -> f(g(x))
+
+"""
+BB - Bluebird^2
+"""
+bb = f -> g -> h -> x -> f(g(h(x)))
+
+"""
+BBB - Bluebird^3
+"""
+bbb = f -> g -> h -> i -> x -> f(g(h(i(x))))
+
+"""
+B1 - Blackbird
+"""
+b1 = f -> g -> x -> y -> f(g(x)(y))
+
+"""
+C - Cardinal
+"""
+c = f -> x -> y -> f(y)(x)
+
+"""
+D2 - Dovekie
+"""
+d2 = f -> g -> h -> x -> y -> g(f(x))(h(y))
+
+"""
+KI - Kite
+"""
+ki = x -> y -> y
+
+"""
+Phi - Phoenix
+"""
+phi = f -> g -> h -> x -> g(f(x))(h(x))
+
+"""
+Phi1 - Pheasant
+"""
+phi1 = f -> g -> h -> x -> y -> g(f(x)(y))(h(x)(y))
+
+"""
+Psi - Psi Bird
+"""
+psi = f -> g -> x -> y -> f(g(x))(g(y))
+
+"""
+W - Warbler
+"""
+w = f -> x -> f(x)(x)
+
+export i, k, s, b, bb, bbb, b1, c, d2, ki, phi, phi1, psi, w
+
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,90 @@
 using Kestrel
+using Kestrel.Curried 
 using Test
 
+const K = Kestrel
+const KC = Kestrel.Curried
+
 @testset "Kestrel.jl" begin
-    # Write your tests here.
+
+    # I
+    @test K.i(1) == 1
+    @test K.i(2) != 1
+    # K
+    @test K.k(1, 2) == 1
+    @test K.k(1, 2) != 2
+    # S
+    @test K.s(K.k, K.i)(1) == 1
+    @test K.s((x, y) -> x + y, K.i)(1) == 2
+    # B
+    @test K.b(K.i, K.i)(1) == 1
+    @test K.b((x) -> x + 1, (x) -> x * 2)(1) == 3
+    # BB
+    @test K.bb(K.i, K.i, K.i)(1) == 1
+    @test K.bb((x) -> x + 1, (x) -> x * 2, (x) -> x ^ 3)(1) == 3
+    # BBB
+    @test K.bbb(K.i, K.i, K.i, K.i)(1) == 1
+    @test K.bbb((x) -> x + 1, (x) -> x * 2, (x) -> x ^ 3, (x) -> x + 1)(1) == 17
+    # B1
+    @test K.b1(K.i, K.k)(1, 2) == 1
+    @test K.b1((x) -> x + 1, (x, y) -> x + y)(1, 2) == 4
+    # C
+    @test K.c(K.k)(1, 2) == 2
+    @test K.c(K.k)(1, 2) != 1
+    # D2
+    @test K.d2(K.i, K.k, K.i)(1, 2) == 1
+    @test K.d2(K.i, K.k, K.i)(1, 2) != 2
+    # KI
+    @test K.ki(1, 2) != 1
+    @test K.ki(1, 2) == 2
+    # Phi
+    @test K.phi(K.i, K.k, K.i)(1) == 1
+    # Phi1
+    @test K.phi1(K.k, K.k, K.ki)(1, 2) == 1
+    # Psi
+    @test K.psi(K.k, K.i)(1, 2) == 1
+    # W
+    @test K.w(K.k)(1) == 1
+
+    # Curried
+
+    # I
+    @test KC.i(1) == 1
+    @test KC.i(2) != 1
+    # K
+    @test KC.k(1)(2) == 1
+    @test KC.k(1)(2) != 2
+    # S
+    @test KC.s(KC.k)(KC.i)(1) == 1
+    @test KC.s(x -> y -> x + y)(KC.i)(1) == 2
+    # B
+    @test KC.b(KC.i)(KC.i)(1) == 1
+    @test KC.b((x) -> x + 1)((x) -> x * 2)(1) == 3
+    # BB
+    @test KC.bb(KC.i)(KC.i)(KC.i)(1) == 1
+    @test KC.bb((x) -> x + 1)((x) -> x * 2)((x) -> x ^ 3)(1) == 3
+    # BBB
+    @test KC.bbb(KC.i)(KC.i)(KC.i)(KC.i)(1) == 1
+    @test KC.bbb((x) -> x + 1)((x) -> x * 2)((x) -> x ^ 3)((x) -> x + 1)(1) == 17
+    # B1
+    @test KC.b1(KC.i)(KC.k)(1)(2) == 1
+    @test KC.b1((x) -> x + 1)(x -> y -> x + y)(1)(2) == 4
+    # C
+    @test KC.c(KC.k)(1)(2) == 2
+    @test KC.c(KC.k)(1)(2) != 1
+    # D2
+    @test KC.d2(KC.i)(KC.k)(KC.i)(1)(2) == 1
+    @test KC.d2(KC.i)(KC.k)(KC.i)(1)(2) != 2
+    # KI
+    @test KC.ki(1)(2) != 1
+    @test KC.ki(1)(2) == 2
+    # Phi
+    @test KC.phi(KC.i)(KC.k)(KC.i)(1) == 1
+    # Phi1
+    @test KC.phi1(KC.k)(KC.k)(KC.ki)(1)(2) == 1
+    # Psi
+    @test KC.psi(KC.k)(KC.i)(1)(2) == 1
+    # W
+    @test KC.w(KC.k)(1) == 1
+
 end
